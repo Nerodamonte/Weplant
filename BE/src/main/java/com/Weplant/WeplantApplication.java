a6ad254b2e00617e38cabb0595c730c238f891c2
Uông Đình Dũng <dungudse183885@fpt.edu.vn>
2025-08-30 00:26:23 +0700

    init BE

---
+++ BE/src/main/java/com/Weplant/WeplantApplication.java
@@ -0,0 +1,13 @@
+package com.Weplant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeplantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeplantApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/Weplant/WeplantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Weplant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeplantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
