8c8c5c4cd0eb7b6b805fb0e72f425a66ddf8cc70
Uông Đình Dũng <dungudse183885@fpt.edu.vn>
2025-09-09 14:50:13 +0700

    add exceptionhandler

---
+++ BE/src/main/java/com/Weplant/exceptions/ErrorCode.java
@@ -0,0 +1,28 @@
+package com.Weplant.exceptions;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public enum ErrorCode {
+
+    NOT_FOUND_USER(404, "Not found user", HttpStatus.NOT_FOUND),
+    WRONG_PASSWORD(400, "Wrong password", HttpStatus.BAD_REQUEST),
+    INVALID_TOKEN(401, "Invalid or expired token", HttpStatus.UNAUTHORIZED),
+    UNAUTHENTICATED(401, "Unauthenticated", HttpStatus.UNAUTHORIZED),
+    BAD_REQUEST(500, "BAD_REQUEST", HttpStatus.BAD_REQUEST);;
+
+    int code;
+    String message;
+    HttpStatusCode statusCode;
+
+    ErrorCode(int code, String message, HttpStatusCode statusCode) {
+        this.code = code;
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+}
